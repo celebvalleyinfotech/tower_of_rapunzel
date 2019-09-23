bfb79af3e94bd6d5701f58b404c362f1f75048be
Tundish <tundish@gigeconomy.org.uk>
2019-09-23 13:17:21 +0100

    Fix double-fall bug in rules sim.

---
+++ CHANGELOG.rst
@@ -0,0 +1,35 @@
+Change Log
+::::::::::
+
+Versions
+========
+
+
+Diary
+=====
+
+2019-09-23
+----------
+
+About three hours in. The basic idea came quickly, and
+then my brain immediately complicated it with all sorts of
+pointless embellishments.
+
+I tried to draw it out on paper. Decided on a world with
+8 separate locations, and then soon realised you'd have
+to jump back and forth between them pretty quickly.
+
+I need to be sure that the game is viable from the outset.
+There'll be no time to build it and fix it later. All my
+code so far is an attempt to simulate the thing to see if
+a satisfying end state can be reached in a reasonable
+amount of time.
+
+After tweaking some rules it seems there should be a final
+Gold/Silver/Bronze outcome. And you'll have to take a risk
+to get the top prize.
+
+Next steps are:
+
+    * build out the game rules from my simulation
+    * put in the framework for web interaction
--- tor/rules.py
+++ tor/rules.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+# This file is part of Tower of Rapunzel.
+#
+# Tower of Rapunzel is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Tower of Rapunzel is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Tower of Rapunzel.  If not, see <http://www.gnu.org/licenses/>.
+
+from collections import namedtuple
+from collections import OrderedDict
+import random
 from collections import namedtuple
 import fractions
 import itertools
@@ -39,8 +57,9 @@ def operate(coins=coins, cut=cut, growth=growth, health=health_max, length=lengt
             cut = max(0, cut + choice)
             length = max(0, length - cut)
         elif locn == "F":
-            damage = health_drop * (tower - length)
-            health = max(0, health - max(0, damage))
+            if n == 1:
+                damage = health_drop * (tower - length)
+                health = max(0, health - max(0, damage))
             if health == 0:
                 return
         elif locn == "W":
