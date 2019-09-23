94b514615a99b3df061c1eb777ad4d43c393a136
Tundish <tundish@gigeconomy.org.uk>
2019-09-23 07:14:32 +0100

    Quickly add remaining rules.

--- tor/rules.py
+++ tor/rules.py
@@ -9,22 +9,22 @@ strategy = "BFWHGFBC"
 
 tower = 12
 playtime_s = 20 * 60
-health = 100
+health_max = 100
 coins = 0
 growth = fractions.Fraction(1, 80)
 length = 12
 cut = 1
 delta = 1
 
-health_drop = fractions.Fraction(length / health)
+health_drop = fractions.Fraction(length / health_max)
 coin_for_hair = 1  # per metre
 coin_for_health = fractions.Fraction(1, 40)
-coin_for_growth = fractions.Fraction(1, 5)
+growth_for_coin = fractions.Fraction(1, 400)
 
 def pathway(n_cycles=int(playtime_s / 9 / len(strategy))):
     return itertools.chain.from_iterable(itertools.repeat(strategy, n_cycles))
 
-def operate(coins=coins, cut=cut, health=health, length=length):
+def operate(coins=coins, cut=cut, growth=growth, health=health_max, length=length):
     for n, locn in enumerate(pathway()):
         if locn == "C":
             # Rapunzel's hair is ?m long. How much do you want to cut?
@@ -36,6 +36,16 @@ def operate(coins=coins, cut=cut, health=health, length=length):
             health = max(0, health - max(0, health_drop * (tower - length)))
         elif locn == "W":
             coins += coin_for_hair * cut
+        elif locn == "H":
+            cost = min(coins, int((health_max - health) * coin_for_health))
+            coins -= cost
+            health += cost / coin_for_health
+        elif locn == "G":
+            choice = random.choice([i * coin_for_hair for i in (0, 1, 2, 5)])
+            cost = min(coins, choice)
+            coins -= cost
+            growth += cost * growth_for_coin
+
         length += length * growth
         yield (n, locn, int(length), coins, int(health), cut)
 
